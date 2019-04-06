2e46d4dcf494a60c080eb4d9cfa68bc32f619d01
bojangabric <bojan@bojangabric.com>
2019-04-06 20:15:59 +0200

    Add new note

--- src/App.js
+++ src/App.js
@@ -1,31 +1,100 @@
 import React, { Component } from 'react';
 import Note from './Note';
 import './App.css';
+import NotePopup from './NotePopup';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [
-        {
-          title: 'Titledsadsada',
-          text: '# Text'
-        }
-      ]
+      newNote: false,
+      editMode: false,
+      notes: JSON.parse(localStorage.getItem('notes')) || []
     };
+    this.newNote = this.newNote.bind(this);
+    this.toggleNewNote = this.toggleNewNote.bind(this);
+    this.toggleEditMode = this.toggleEditMode.bind(this);
+    this.saveNote = this.saveNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+  }
+
+  newNote() {
+    this.setState({
+      newNote: true,
+      editMode: true
+    })
+  }
+
+  toggleEditMode() {
+    this.setState({
+      editMode: !this.state.editMode
+    });
+  }
+
+  toggleNewNote() {
+    this.setState({
+      newNote: !this.state.newNote,
+      editMode: false
+    });
+  }
+
+  saveNote(note, index = -1) {
+    if (index === -1) {
+      this.setState({
+        notes: [...this.state.notes, note],
+        newNote: false
+      }, () => (localStorage.setItem('notes', JSON.stringify(this.state.notes))));
+    } else {
+      let n = [...this.state.notes];
+      n[index] = note;
+      this.setState({
+        notes: n,
+        newNote: false
+      }, () => (localStorage.setItem('notes', JSON.stringify(this.state.notes))));
+    }
+  }
+
+  deleteNote(index) {
+    let n = [...this.state.notes];
+    n.splice(index, 1);
+    this.setState({
+      notes: n
+    }, () => (localStorage.setItem('notes', JSON.stringify(this.state.notes))));
   }
 
   render() {
+    const editOn = this.state.editMode ? 'edit-on' : '';
+
     const notes = this.state.notes.map((note, i) =>
       <Note
+        editOn={editOn}
         title={note.title}
         text={note.text}
+        save={this.saveNote}
+        edit={this.toggleEditMode}
+        delete={this.deleteNote}
+        index={i}
         key={i}
       />
     );
 
+    let newnote = null;
+    if (this.state.newNote) {
+      newnote = <NotePopup
+        editOn={editOn}
+        title='Placeholder'
+        text='Placeholder'
+        save={this.saveNote}
+        edit={this.toggleEditMode}
+        delete={this.toggleNewNote}
+        close={this.toggleNewNote}
+      />
+    }
+
     return (
       <div className='App'>
+        <button className='new-note' onClick={this.newNote}>New note</button>
+        {newnote}
         {notes}
       </div>
     );
